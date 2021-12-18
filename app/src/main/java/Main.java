21fc6f4a3a7bcef5c74e722a78cc568b122dff71
Hikari Ishihara <e205217@ie.u-ryukyu.ac.jp>
2021-12-18 09:37:42 +0900

    Playerの追加

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -1,3 +1,14 @@
+import jp.ac.uryukyu.ie.e205217.*;
+import java.util.*;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        Player player1 = new Player("1", 0);
+        Player player2 = new Player("2", 0);    
+
+        System.out.println("おみくじすごろくゲーム開始です！");
+        new Scanner(System.in).nextLine();
+        player1.to_roku();
+        player2.to_roku();
+    }
 }
---
+++ app/src/main/java/jp/ac/uryukyu/ie/e205217/Player.java
@@ -0,0 +1,15 @@
+package jp.ac.uryukyu.ie.e205217;
+
+public class Player {
+    public String name;
+    public int position;
+
+    public Player(String name, int position){
+        this.name = name;
+        this.position = position;
+    }   
+    public void to_roku(){
+        System.out.printf("%sさん、登録完了。%dマス目からスタート！\n", name, position);
+    }
+    
+}
